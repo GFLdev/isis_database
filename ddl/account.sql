cc59b4d9f28596f03c651382d603a8d54f63e47b
Gabriel Franco <gabriel.franco@gfldev.com>
2025-07-01 18:05:22 -0300

    v1.1: refactored RBAC tables from Account DDL

--- ddl/account.sql
+++ ddl/account.sql
@@ -4,61 +4,92 @@ CREATE SCHEMA account;
 --- Custom Types ---
 --------------------
 
-CREATE TYPE SERVICE_NAME AS ENUM ('account', 'finance', 'drive');
-COMMENT ON TYPE SERVICE_NAME IS 'Service name';
+CREATE TYPE module_name AS ENUM ('account', 'finance', 'drive', 'calendar', 'ai');
+COMMENT ON TYPE module_name IS 'Module name';
 
-CREATE TYPE TABLE_NAME AS ENUM ('account', 'account_group', 'group_permission', 'refresh_token', 'login_attempt');
-COMMENT ON TYPE TABLE_NAME IS 'Table name';
+CREATE TYPE table_name AS ENUM ('account', 'role', 'role_module', 'module','refresh_token', 'login_attempt');
+COMMENT ON TYPE table_name IS 'Table name';
 
-CREATE TYPE QUERY_TYPE AS ENUM ('insert', 'select' , 'update', 'delete');
-COMMENT ON TYPE QUERY_TYPE IS 'Query type';
+CREATE TYPE query_type AS ENUM ('insert', 'select' , 'update', 'delete');
+COMMENT ON TYPE query_type IS 'Query type';
 
-CREATE TYPE LOG_TYPE AS ENUM ('success','error');
-COMMENT ON TYPE LOG_TYPE IS 'Log type';
+CREATE TYPE log_type AS ENUM ('success','error');
+COMMENT ON TYPE log_type IS 'Log type';
 
----------------------
---- ACCOUNT_GROUP ---
----------------------
+------------
+--- ROLE ---
+------------
 
-CREATE TABLE account.account_group
-(
-    account_group_id UUID PRIMARY KEY     DEFAULT gen_random_uuid(),
-    name             VARCHAR(50) NOT NULL,
-    description      VARCHAR(1000),
-    created_at       TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    modified_at      TIMESTAMPTZ
+CREATE TABLE account.role (
+    role_id     uuid PRIMARY KEY     DEFAULT gen_random_uuid(),
+    name        VARCHAR(50) NOT NULL,
+    description VARCHAR(1000),
+    created_at  timestamptz NOT NULL DEFAULT NOW(),
+    modified_at timestamptz
 );
-COMMENT ON TABLE account.account_group IS 'Account group table';
-COMMENT ON COLUMN account.account_group.account_group_id IS 'Account group id';
-COMMENT ON COLUMN account.account_group.name IS 'Account group''s name';
-COMMENT ON COLUMN account.account_group.description IS 'Account group''s description';
-COMMENT ON COLUMN account.account_group.created_at IS 'Account group''s creation date';
-COMMENT ON COLUMN account.account_group.modified_at IS 'Account group''s last modification date';
+COMMENT ON TABLE account.role IS 'Role table';
+COMMENT ON COLUMN account.role.role_id IS 'Role id';
+COMMENT ON COLUMN account.role.name IS 'Role''s name';
+COMMENT ON COLUMN account.role.description IS 'Role''s description';
+COMMENT ON COLUMN account.role.created_at IS 'Role''s creation date';
+COMMENT ON COLUMN account.role.modified_at IS 'Role''s last modification date';
+
+--------------
+--- MODULE ---
+--------------
+
+CREATE TABLE account.module (
+    module_id   uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    name        module_name NOT NULL,
+    description VARCHAR(1000)
+);
+COMMENT ON TABLE account.module IS 'Module table';
+COMMENT ON COLUMN account.module.module_id IS 'Module id';
+COMMENT ON COLUMN account.module.name IS 'Module''s name';
+COMMENT ON COLUMN account.module.description IS 'Module''s description';
+
+-------------------
+--- ROLE_MODULE ---
+-------------------
+
+CREATE TABLE account.role_module (
+    role_id   uuid    NOT NULL,
+    module_id uuid    NOT NULL,
+    elevated  BOOLEAN NOT NULL DEFAULT FALSE,
+    FOREIGN KEY (role_id)
+        REFERENCES account.role (role_id) ON DELETE CASCADE,
+    FOREIGN KEY (module_id)
+        REFERENCES account.module (module_id) ON DELETE CASCADE
+);
+COMMENT ON TABLE account.role_module IS 'Role-Module reference table';
+COMMENT ON COLUMN account.role_module.role_id IS 'Role id';
+COMMENT ON COLUMN account.role_module.module_id IS 'Module id';
+COMMENT ON COLUMN account.role_module.elevated IS 'Elevated (admin) permission flag';
 
 ---------------
 --- ACCOUNT ---
 ---------------
 
-CREATE TABLE account.account
-(
-    account_id       UUID PRIMARY KEY             DEFAULT gen_random_uuid(),
-    account_group_id UUID                NOT NULL,
-    username         VARCHAR(30) UNIQUE  NOT NULL,
-    name             VARCHAR(100)        NOT NULL,
-    surname          VARCHAR(100)        NOT NULL,
-    email            VARCHAR(100) UNIQUE NOT NULL,
-    password         VARCHAR(72)         NOT NULL,
-    is_active        BOOLEAN             NOT NULL DEFAULT TRUE,
-    login_count      INTEGER             NOT NULL DEFAULT 0,
-    last_login_at    TIMESTAMPTZ,
-    created_at       TIMESTAMPTZ         NOT NULL DEFAULT NOW(),
-    modified_at      TIMESTAMPTZ,
-    FOREIGN KEY (account_group_id) REFERENCES account.account_group (account_group_id) ON DELETE CASCADE,
+CREATE TABLE account.account (
+    account_id    uuid PRIMARY KEY             DEFAULT gen_random_uuid(),
+    role_id       uuid                NOT NULL,
+    username      VARCHAR(30) UNIQUE  NOT NULL,
+    name          VARCHAR(100)        NOT NULL,
+    surname       VARCHAR(100)        NOT NULL,
+    email         VARCHAR(100) UNIQUE NOT NULL,
+    password      VARCHAR(72)         NOT NULL,
+    is_active     BOOLEAN             NOT NULL DEFAULT TRUE,
+    login_count   INTEGER             NOT NULL DEFAULT 0,
+    last_login_at timestamptz,
+    created_at    timestamptz         NOT NULL DEFAULT NOW(),
+    modified_at   timestamptz,
+    FOREIGN KEY (role_id)
+        REFERENCES account.role (role_id) ON DELETE CASCADE,
     CONSTRAINT username_length CHECK (CHAR_LENGTH(username) >= 4)
 );
 COMMENT ON TABLE account.account IS 'Account table';
 COMMENT ON COLUMN account.account.account_id IS 'Account''s id';
-COMMENT ON COLUMN account.account.account_group_id IS 'Account''s group id';
+COMMENT ON COLUMN account.account.role_id IS 'Account''s role id';
 COMMENT ON COLUMN account.account.username IS 'Account''s username';
 COMMENT ON COLUMN account.account.name IS 'Account''s user first name';
 COMMENT ON COLUMN account.account.surname IS 'Account''s user surname';
@@ -70,36 +101,16 @@ COMMENT ON COLUMN account.account.last_login_at IS 'Account''s last login date';
 COMMENT ON COLUMN account.account.created_at IS 'Account''s creation date';
 COMMENT ON COLUMN account.account.modified_at IS 'Account''s last modification date';
 
-------------------------
---- GROUP_PERMISSION ---
-------------------------
-
-CREATE TABLE account.group_permission (
-    group_permission_id uuid PRIMARY KEY      DEFAULT gen_random_uuid(),
-    account_group_id    uuid         NOT NULL,
-    service_name        service_name NOT NULL,
-    is_able             BOOLEAN      NOT NULL DEFAULT TRUE,
-    is_admin            BOOLEAN      NOT NULL DEFAULT FALSE,
-    FOREIGN KEY (account_group_id)
-        REFERENCES account.account_group (account_group_id) ON DELETE CASCADE
-);
-COMMENT ON TABLE account.group_permission IS 'Group permission table';
-COMMENT ON COLUMN account.group_permission.group_permission_id IS 'Group permission''s id';
-COMMENT ON COLUMN account.group_permission.account_group_id IS 'Account group linked to group permission';
-COMMENT ON COLUMN account.group_permission.service_name IS 'Service linked to group permission';
-COMMENT ON COLUMN account.group_permission.is_able IS 'If the group permission is able to access the module';
-COMMENT ON COLUMN account.group_permission.is_admin IS 'If the group permission is an admin';
-
 ---------------------
 --- REFRESH_TOKEN ---
 ---------------------
 
-CREATE TABLE account.refresh_token
-(
-    refresh_token_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    account_id       UUID        NOT NULL,
-    expiration_date  TIMESTAMPTZ NOT NULL,
-    FOREIGN KEY (account_id) REFERENCES account.account (account_id) ON DELETE CASCADE
+CREATE TABLE account.refresh_token (
+    refresh_token_id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    account_id       uuid        NOT NULL,
+    expiration_date  timestamptz NOT NULL,
+    FOREIGN KEY (account_id)
+        REFERENCES account.account (account_id) ON DELETE CASCADE
 );
 COMMENT ON TABLE account.refresh_token IS 'Refresh token table';
 COMMENT ON COLUMN account.refresh_token.refresh_token_id IS 'Refresh token''s id, used as the token itself';
@@ -110,15 +121,15 @@ COMMENT ON COLUMN account.refresh_token.expiration_date IS 'Refresh token''s exp
 --- LOGIN_ATTEMPT ---
 ---------------------
 
-CREATE TABLE account.login_attempt
-(
-    login_attempt_id UUID PRIMARY KEY     DEFAULT gen_random_uuid(),
-    account_id       UUID        NOT NULL,
-    attempted_at     TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+CREATE TABLE account.login_attempt (
+    login_attempt_id uuid PRIMARY KEY     DEFAULT gen_random_uuid(),
+    account_id       uuid        NOT NULL,
+    attempted_at     timestamptz NOT NULL DEFAULT NOW(),
     success          BOOLEAN     NOT NULL,
     ip_address       VARCHAR(15) NOT NULL,
     user_agent       TEXT        NOT NULL,
-    FOREIGN KEY (account_id) REFERENCES account.account (account_id) ON DELETE CASCADE
+    FOREIGN KEY (account_id)
+        REFERENCES account.account (account_id) ON DELETE CASCADE
 );
 COMMENT ON TABLE account.login_attempt IS 'Login attempt''s log table';
 COMMENT ON COLUMN account.login_attempt.login_attempt_id IS 'Login attempt''s id';
@@ -132,16 +143,15 @@ COMMENT ON COLUMN account.login_attempt.user_agent IS 'Login attempt''s user age
 --- ACCOUNT_LOG ---
 -------------------
 
-CREATE TABLE account.account_log
-(
-    log_id      UUID PRIMARY KEY       DEFAULT gen_random_uuid(),
-    table_name  TABLE_NAME    NOT NULL,
+CREATE TABLE account.account_log (
+    log_id      uuid PRIMARY KEY       DEFAULT gen_random_uuid(),
+    table_name  table_name    NOT NULL,
     table_field VARCHAR(50)   NOT NULL,
     pk_value    VARCHAR(36)   NOT NULL,
-    query_type  QUERY_TYPE    NOT NULL,
-    log_type    LOG_TYPE      NOT NULL,
+    query_type  query_type    NOT NULL,
+    log_type    log_type      NOT NULL,
     message     VARCHAR(1000) NOT NULL,
-    created_at  TIMESTAMPTZ   NOT NULL DEFAULT NOW()
+    created_at  timestamptz   NOT NULL DEFAULT NOW()
 );
 COMMENT ON TABLE account.account_log IS 'Account log table';
 COMMENT ON COLUMN account.account_log.log_id IS 'Account log''s id';
